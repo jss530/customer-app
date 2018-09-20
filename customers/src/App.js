af20d5e8e0e24381298a5dc20fd8bc3188974313
jss530 <jsifre01@gmail.com>
2018-09-20 13:33:11 -0600

    Started coding file import functions

--- customers/src/App.js
+++ customers/src/App.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  const ImportFromFile = () => {
+    let fileReader;
+
+    const handleFileRead = (e) => {
+      const content = fileReader.result;
+      //finish content action here
+    };
+
+    const handleFileChosen = (file) => {
+      fileReader = new FileReader();
+      fileReader.onloadend = handleFileRead;
+      fileReader.readAsText(file);
+    };
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to the Kustomer Guest Import Page!</h1>
         </header>
+
         <p className="App-intro">
-          Click here to import your file into Kustomer.
         </p>
+
+        <div className="upload">
+          <input type='file' id='file' className='input-file' accept='.csv'
+          onChange={e => handleFileChosen(e.target.files[0])} />
+        </div>
       </div>
     );
   }
