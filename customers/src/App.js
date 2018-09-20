c78f5029814a05c4917637faee91f29a3cd00faa
jss530 <jsifre01@gmail.com>
2018-09-20 12:45:25 -0600

    Created initial app

---
+++ customers/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to the Kustomer Guest Import Page!</h1>
+        </header>
+        <p className="App-intro">
+          Click here to import your file into Kustomer.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
