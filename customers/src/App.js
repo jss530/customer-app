f4ea4b1327f9b0de33e01e9af5ddfd0b798f24ff
jss530 <jsifre01@gmail.com>
2018-09-21 20:03:35 -0400

    started creating import function

--- customers/src/App.js
+++ customers/src/App.js
@@ -1,41 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-// import {CSV}
 
 class App extends Component {
 
-// let records = (need to import your CSV here)
+  importFromFile = csv => {
+    let csv =require('./files/data.csv');
+    let output = csv.map(record => {
+    let arr = record.split(',')
 
-let output = records.map(record => {
-
-	let arr = record.split(',')
-
-	return
-  {"name": arr[0],
-    "emails": [
-        {
-          "type": "home",
-          "email": arr[1],
-        }
-      ],
-    "phones": [
-        {
-          "type": "home",
-          "phone": parseInt(arr[2])
-        },
-        {
-          "type": "work",
-          "phone": parseInt(arr[3])
-        }
-      ],
-     "birthdayAt": arr[4],
-     "tags": arr[5]
-    };
-
-	}
-})
-
-console.log(output)
+  	return {
+      "name": arr[0],
+      "emails": [
+          {
+            "type": "home",
+            "email": arr[1],
+          }
+        ],
+      "phones": [
+          {
+            "type": "home",
+            "phone": parseInt(arr[2])
+          },
+          {
+            "type": "work",
+            "phone": parseInt(arr[3])
+          }
+        ],
+       "birthdayAt": arr[4],
+       "tags": arr[5]
+      };
+  	});
+  };
 
   render() {
     return (
